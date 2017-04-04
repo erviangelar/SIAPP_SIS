b6bac59d2000ebd7a5b056b6205cdc7f31c1b258
erviangelar <erviangelar@gmail.com>
2017-04-04 10:57:06 +0700

    update

--- SIApps/src/app/app.component.ts
+++ SIApps/src/app/app.component.ts
@@ -6,7 +6,6 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 //import { HomePage } from '../pages/home/home';
 import { LoginPage } from '../pages/login/login';
 
-
 @Component({
   templateUrl: 'app.html'
 })
