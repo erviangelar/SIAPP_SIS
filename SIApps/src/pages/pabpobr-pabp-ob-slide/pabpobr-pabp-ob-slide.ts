1ac6055d644ec60b2b1c04859ca5dc3884749983
erviangelar <erviangelar@gmail.com>
2017-04-04 16:33:34 +0700

    MAntap

---
+++ SIApps/src/pages/pabpobr-pabp-ob-slide/pabpobr-pabp-ob-slide.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+
+@Component({
+  selector: 'page-pabpobr-pabp-ob-slide',
+  templateUrl: 'pabpobr-pabp-ob-slide.html'
+})
+export class PabpobrPabpObSlidePage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad PabpobrPabpObSlidePage');
+  }
+
+}
+
+
---
+++ SIApps/src/pages/pabpobr-pabp-ptr-slide/pabpobr-pabp-ptr-slide.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+/*
+  Generated class for the PabpobrPabpPtrSlide page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  selector: 'page-pabpobr-pabp-ptr-slide',
+  templateUrl: 'pabpobr-pabp-ptr-slide.html'
+})
+export class PabpobrPabpPtrSlidePage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad PabpobrPabpPtrSlidePage');
+  }
+
+}
